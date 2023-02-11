0cedeea58fd9361fafae309c28237836b04573c0
Chiya <spam@chiya.ru>
2023-02-11 04:07:59 +0300

    added: hardcoded code xD

--- app/src/main/java/ru/chiya/clounada/utils/BruhData.kt
+++ app/src/main/java/ru/chiya/clounada/utils/BruhData.kt
@@ -5,6 +5,16 @@ import kotlinx.serialization.json.*
 import ru.chiya.clounada.R
 import java.io.*
 
+fun getListOfPlaces(theatre: String): List<String> {
+    if (theatre == "TYZ") {
+        return listOf<String>("Балкон", "Партер")
+    } else if (theatre == "TD"){
+        return listOf<String>("Балкон, центр", "Правый балкон", "Левый балкон", "Бенуар, центр", "Правый бенуар", "Левый бенуар", "Бельэтаж, центр", "Правый бельэтаж ", "Левый бельэтаж", "Партер" )
+    } else if (theatre == "Circus"){
+        return listOf<String>("Правая сторона, 1 сектор", "Правая сторона, 2 сектор", "Правая сторона, 3 сектор","Правая сторона, 4 сектор","Левая сторона, 1 сектор","Левая сторона, 2 сектор", "Левая сторона, 3 сектор","Левая сторона, 4 сектор")
+    }
+    return listOf<String>("")
+}
 
 fun readRawTextFile(ctx: Context, resId: Int): String? {
     val inputStream: InputStream = ctx.getResources().openRawResource(resId)
